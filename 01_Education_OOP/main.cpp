a180d8488bca689c095ab8609b3ca0e5ace820da
Denis-Bez <v417459@yandex.ru>
2022-06-14 14:48:24 +0500

    Education C++ OOP

--- 01_Education_OOP/main.cpp
+++ 01_Education_OOP/main.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+/* Также вариант (https://www.youtube.com/watch?v=rh9U1duQR8s&list=WL&index=29&t=13s)  с использованием приватного модификатора доступа
+    и использованием функций get и set */
 class User {
 public:
     string name;
@@ -15,18 +17,29 @@ public:
         age = _age;
         adress = _adress;
     }
-
-    // Метод
-    void Display() {
-        cout << "Имя: " << name << " Возраст: " << age << " Адрес: " << adress
+    
+    // Деструктор автоматически вызывается, когда удаляется объект
+    ~User() {
+        cout << "Distructor called";
     }
+    
+    // Метод
+/*     void Display() {
+        cout << "Name: " << name << " Age: " << age << " Adress: " << adress;
+    } */
 };
 
 int main() {
     
+    setlocale(LC_ALL, "RU");
+
     // Создание объекта
-    User _user("Tom", 19, "London");
-    _user.Display();
+/*     User _user("Tom", 19, "London");
+    _user.Display(); */
+
+    // Создаем динамический объект
+    User *_user = new User("Tom", 19, "London");
+    delete _user; // После удаления объекта будет вызван деструктор
 
     return 0;
 }
